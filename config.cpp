2282b19b8eea163ddd2cf3fc1d1eed1024a511dc
Talford1004 <67633537+Talford1004@users.noreply.github.com>
2020-06-30 10:59:12 +0100

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,268 @@
+class CfgPatches
+{
+	class 14aag_Main
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={};
+	};
+};
+class cfgFactionClasses
+{
+	class 14aag_FACTION
+	{
+		displayName="14AAG";
+		priority=1;
+		side=1;
+		icon="";
+	};
+	class 14aag_FACTION_EN
+	{
+		displayName="14AAG";
+		priority=1;
+		side=0;
+		icon="";
+	};
+	class 14aag_FACTION_Stat
+	{
+		displayName="14AAG";
+		priority=1;
+		side=3;
+		icon="";
+	};
+};
+class cfgVehicleClasses
+{
+	class My_Subgroup_Men
+	{
+		displayName="Men";
+	};
+	class My_Subgroup_Cars
+	{
+		displayName="Cars";
+	};
+	class My_Subgroup_IFV
+	{
+		displayName="IFV";
+	};
+	class My_Subgroup_APC
+	{
+		displayName="APCs";
+	};
+	class My_Subgroup_Helis
+	{
+		displayName="Helicopters";
+	};
+	class My_Subgroup_Boats
+	{
+		displayName="Boats";
+	};
+	class My_Subgroup_Launchers
+	{
+		displayName="Launchers";
+	};
+	class My_Subgroup_Planes
+	{
+		displayName="Planes";
+	};
+	class My_Subgroup_Radars
+	{
+		displayName="Radars";
+	};
+	class My_Subgroup_Airfield
+	{
+		displayName="Airfield";
+	};
+	class My_Subgroup_Ammunition
+	{
+		displayName="Ammunition";
+	};
+	class My_Subgroup_Supplies
+	{
+		displayName="Supplies";
+	};
+	class My_Subgroup_Medical
+	{
+		displayName="Medical";
+	};
+	class My_Subgroup_Armoury
+	{
+		displayName="Armoury";
+	};
+};
+class CfgEditorCategories
+{
+	class 14_aag_Eden_Category
+	{
+		displayName="14AAG";
+	};
+	class 14_aag_Civ_Eden_Category
+	{
+		displayName="14AAG Civilian";
+	};
+	class 14_aag_Opfor_Eden_Category
+	{
+		displayName="14AAG OPFOR";
+	};
+	class 14_aag_GreenFor_Eden_Category
+	{
+		displayName="14AAG Greenfor";
+	};
+	class 14_aag_Static_Eden_Category
+	{
+		displayName="14AAG Static";
+	};
+};
+class CfgEditorSubcategories
+{
+	class 14_aag_Eden_Subcategory_Equipment_Headgear
+	{
+		displayName="14AAG Headgear";
+	};
+	class 14_aag_Eden_Subcategory_Equipment_Vest
+	{
+		displayName="14AAG Vests";
+	};
+	class 14_aag_Eden_Subcategory_Equipment_Uniform
+	{
+		displayName="14AAG Uniforms";
+	};
+	class 14_aag_Eden_Subcategory_Equipment_Backpack
+	{
+		displayName="14AAG Backpacks";
+	};
+	class 14_aag_Eden_Subcategory_Crate_Ammuniton
+	{
+		displayName="Ammunition";
+	};
+	class 14_aag_Eden_Subcategory_Crate_Supplies
+	{
+		displayName="Supplies";
+	};
+	class 14_aag_Eden_Subcategory_Crate_Medical
+	{
+		displayName="Medical";
+	};
+	class 14_aag_Eden_Subcategory_Crate_Armoury
+	{
+		displayName="Armoury";
+	};
+	class 14_aag_Eden_Subcategory_Object_fob
+	{
+		displayName="14AAG Objects";
+	};
+	class 14_aag_Eden_Subcategory_Object_Targets
+	{
+		displayName="14AAG Targets";
+	};
+	class 14_aag_Eden_Subcategory_Units
+	{
+		displayName="14AAG Units";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Heli
+	{
+		displayName="Helicopters";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Plane
+	{
+		displayName="Planes";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Car
+	{
+		displayName="Cars";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_IFV
+	{
+		displayName="IFV";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_APC
+	{
+		displayName="APC";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Tank
+	{
+		displayName="Tanks";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Boats
+	{
+		displayName="Boats";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Radar
+	{
+		displayName="Radars";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Airfield
+	{
+		displayName="Airfield";
+	};
+	class 14_aag_Eden_Subcategory_Vehicle_Support
+	{
+		displayName="Support Vehicles";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Item
+	{
+		displayName="14AAG Weapons Items";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Attachment
+	{
+		displayName="14AAG Weapons Attachments";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Magazine
+	{
+		displayName="14AAG Weapons Magazines";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Pistol
+	{
+		displayName="14AAG Weapons Pistols";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Rifle
+	{
+		displayName="14AAG Weapons Rifles";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Rifle_Blank
+	{
+		displayName="14AAG Weapons Rifles (Blank)";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Shotgun
+	{
+		displayName="14AAG Weapons Shotguns";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Shotgun_Blank
+	{
+		displayName="14AAG Weapons Shotguns (Blank)";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Launcher
+	{
+		displayName="14AAG Weapons Launchers";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_LMG
+	{
+		displayName="14AAG Weapons Light Machine Guns";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_LMG_Blank
+	{
+		displayName="14AAG Weapons Light Machine Guns (Blank)";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_Static
+	{
+		displayName="14AAG Weapons Static";
+	};
+	class 14_aag_Eden_Subcategory_Weapon_IED
+	{
+		displayName="14AAG Weapons IED";
+	};
+	class 14_aag_Eden_Subcategory_General_Opfor
+	{
+		displayName="General";
+	};
+	class 14_aag_Eden_Subcategory_General_Civ
+	{
+		displayName="General";
+	};
+};
+class cfgMods
+{
+	author="";
+	timepacked="";
+};
